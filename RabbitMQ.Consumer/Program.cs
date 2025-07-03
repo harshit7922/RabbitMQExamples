d18e4d43659ebf0c01fba002dca3223cf57f963d
harshit7922 <harshit.porwal78@gmail.com>
2025-07-04 00:03:55 +0530

    phase 1- adding rabbitmq producer and consumer console app
    rabbitmq example with console application

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! Consumer");
+        Console.WriteLine("Hello, World!");
+        var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:guest@localhost:5672") };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = System.Text.Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Received message: {message}");
+        };
+        channel.BasicConsume(queue: "demo-queue", autoAck: true, consumer: consumer);
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! Producer");
+        var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:guest@localhost:5672") };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+        var message =  new {name = "Test", value = 123};
+        var body = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(message);
+        channel.BasicPublish(exchange: "", routingKey: "demo-queue", null, body: body);
+    }
+}
